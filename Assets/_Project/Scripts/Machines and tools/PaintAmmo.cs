c37a9d99e5b2bb8fb2a4aab41330192dcaa909a1
AliSaidi-Stolenpad <ali.saidi@stolenpad-studio.com>
2023-08-13 15:24:48 +0100

    Paint Liquid Update

--- Assets/_Project/Scripts/Machines and tools/PaintAmmo.cs
+++ Assets/_Project/Scripts/Machines and tools/PaintAmmo.cs
@@ -1,3 +1,4 @@
+using DG.Tweening;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -9,6 +10,7 @@ public class PaintAmmo : MonoBehaviour
     [SerializeField] private float fillRate;
     [SerializeField] private float nextFill;
     [SerializeField] private ParticleSystem effect;
+    private Tween disloveTween;
 
     void Start()
     {
@@ -38,14 +40,28 @@ public class PaintAmmo : MonoBehaviour
     {
         filling = true;
         nextFill = 0;
+        disloveTween.Kill();
         effect.Play();
-        var main = effect.main;
-        main.loop = true;
+        var render = effect.GetComponent<ParticleSystemRenderer>();
+        Material mat = render.material;
+        mat.SetFloat("Disolve", 0.0f);
+        float t = 0.0f;
+        disloveTween = DOTween.To(() => t, x => t = x, 0.5f, 1.5f)
+           .OnUpdate(() => mat.SetFloat("ReverseDisolve", t));
+
+        //var main = effect.main;
+        //main.loop = true;
     }
     private void OnTriggerExit(Collider other)
     {
         filling = false;
-        var main = effect.main;
-        main.loop = false;
+        //var main = effect.main;
+        //main.loop = false;
+        disloveTween.Kill();
+        var render = effect.GetComponent<ParticleSystemRenderer>();
+        Material mat = render.material;
+        float t = 0.0f;
+        disloveTween = DOTween.To(() => t, x => t = x, 0.5f, 1.5f)
+           .OnUpdate(() => mat.SetFloat("Disolve", t)).OnComplete(()=> effect.Stop());
     }
 }
