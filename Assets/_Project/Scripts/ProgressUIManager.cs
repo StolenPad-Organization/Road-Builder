d146c453b4f0924a529a7f6c4e5daecc540904e2
AliSaidi-Stolenpad <ali.saidi@stolenpad-studio.com>
2023-08-25 14:41:08 +0100

    progres bar & stars

--- Assets/_Project/Scripts/ProgressUIManager.cs
+++ Assets/_Project/Scripts/ProgressUIManager.cs
@@ -8,6 +8,8 @@ public class ProgressUIManager : MonoBehaviour
     [SerializeField] private Image barFillImage;
     [SerializeField] private Image iconImage;
     [SerializeField] private Sprite[] icons;
+    [SerializeField] private Image[] starImage;
+    [SerializeField] private Sprite fullstar;
     void Start()
     {
         
@@ -26,5 +28,25 @@ public class ProgressUIManager : MonoBehaviour
     public void UpdateProgressBar(float value)
     {
         barFillImage.fillAmount = value;
+        CheckStars(value);
+    }
+
+    private void CheckStars(float value)
+    {
+        if (value >= 0.34f && value < 0.655f)
+        {
+            starImage[0].sprite = fullstar;
+        }
+        else if (value >= 0.655f && value < 0.95f)
+        {
+            starImage[0].sprite = fullstar;
+            starImage[1].sprite = fullstar;
+        }
+        else if (value >= 0.95f)
+        {
+            starImage[0].sprite = fullstar;
+            starImage[1].sprite = fullstar;
+            starImage[2].sprite = fullstar;
+        }
     }
 }
